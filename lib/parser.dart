67641dce56ff66be9d96792b47d5524ab7c0ae8b
Rasarts <rasart.pro@gmail.com>
2015-06-30 13:08:40 +0300

    update api

--- lib/parser.dart
+++ lib/parser.dart
@@ -1,14 +1,30 @@
-library Parser;
+library mini.parser;
 
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' show parse;
 
-class Parser {
+class MiniParser {
   getContentFrom(String uri, {List selectors}) async {
     var htmlForParse, content, html;
 
     htmlForParse = await http.read(uri);
     html = parse(htmlForParse);
+    content = new Map();
+
+    selectFor(String selector){
+      var contentListFromSelector;
+      List nodes = new List();
+
+      makeMapContent(node){
+        nodes.add(node.text);
+        content[selector] = nodes;
+      }
+
+      contentListFromSelector = html.querySelectorAll(selector);
+      contentListFromSelector.forEach(makeMapContent);
+    }
+
+    selectors.forEach(selectFor);
 
     //content = html.querySelector(selectors).text;
 
